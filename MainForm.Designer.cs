c0d005de003c4acd9315151a58d3d8144d72ba36
KJCD07 <158169042+KJCD07@users.noreply.github.com>
2024-02-05 23:28:55 -0600

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace LinkedServerTestConnectivity
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCheckConnectivity = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(516, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(138, 34);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnCheckConnectivity
+            // 
+            this.btnCheckConnectivity.Location = new System.Drawing.Point(82, 341);
+            this.btnCheckConnectivity.Name = "btnCheckConnectivity";
+            this.btnCheckConnectivity.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckConnectivity.TabIndex = 1;
+            this.btnCheckConnectivity.Text = "btnCheckConnectivity";
+            this.btnCheckConnectivity.UseVisualStyleBackColor = true;
+            this.btnCheckConnectivity.Click += new System.EventHandler(this.btnCheckConnectivity_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheckConnectivity);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCheckConnectivity;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinkedServerTestConnectivity
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Set up the DataGridView
+            dataGridView1.Columns.Add("LinkedServer", "Linked Server");
+            dataGridView1.Columns.Add("Status", "Status");
+        }
+
+        private void btnCheckConnectivity_Click(object sender, EventArgs e)
+        {
+            // Clear previous results
+            dataGridView1.Rows.Clear();
+
+            // Replace "YourServer" with your actual SQL Server instance name
+            ServerConnection serverConnection = new ServerConnection(".");
+
+            try
+            {
+                // Connect to the server
+                Server server = new Server(serverConnection);
+
+                // Iterate through linked servers
+                foreach (LinkedServer linkedServer in server.LinkedServers)
+                {
+                    // Check connectivity
+                    string status = "Connected";
+                    try
+                    {
+                        linkedServer.TestConnection();
+                    }
+                    catch (Exception ex)
+                    {
+                        status = "Error: " + ex.Message;
+                    }
+
+                    // Add data to DataGridView
+                    dataGridView1.Rows.Add(linkedServer.Name, status);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error connecting to server: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Close the connection
+                serverConnection.Disconnect();
+            }
+        }
+
+        
+    }
+}
